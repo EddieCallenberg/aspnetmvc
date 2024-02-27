18a5785998a86e2a21ff7e64fc2ec858b601b68b
Eddie <callenberg99@gmail.com>
2024-02-27 08:35:42 +0100

    Add project files.

---
+++ aspnetmvc/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿const showmenu = () => {
+    if (document.getElementById('mobile-menu').classList.contains('hide')) {
+        document.getElementById('mobile-menu').classList.remove('hide')
+        document.getElementById('mobile-menu-icon').classList.remove('fa-bars')
+        document.getElementById('mobile-menu-icon').classList.add('fa-xmark')
+    } else {
+        document.getElementById('mobile-menu').classList.add('hide')
+        document.getElementById('mobile-menu-icon').classList.remove('fa-xmark')
+        document.getElementById('mobile-menu-icon').classList.add('fa-bars')
+    }
+}
