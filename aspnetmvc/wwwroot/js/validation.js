61c465a26c99a20b4bdad9411b0b2eaa9a478814
Eddie <callenberg99@gmail.com>
2024-03-11 08:38:38 +0100

    signup validation complete

---
+++ aspnetmvc/wwwroot/js/validation.js
@@ -0,0 +1,84 @@
+const formErrorHandler = (element, validationResult) => {
+    let spanElement = document.querySelector(`[data-valmsg-for="${element.name}"]`)
+
+    if (validationResult) {
+        element.classList.remove('input-validation-error')
+        spanElement.classList.remove('field-validation-error')
+        spanElement.classList.add('field-validation-valid')
+        spanElement.innerHTML = ''
+    }
+    else {
+        element.classList.add('input-validation-error')
+        spanElement.classList.add('field-validation-error')
+        spanElement.classList.remove('field-validation-valid')
+        spanElement.innerHTML = element.dataset.valRequired
+    }
+}
+
+const textValidator = (element, minLength = 2) => {
+    if (element.value.length >= minLength) {
+        formErrorHandler(element, true)
+    }
+    else {
+        formErrorHandler(element, false)
+    }      
+}
+
+const emailValidator = (element) => {
+    const regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    formErrorHandler(element, regEx.test(element.value))
+}
+
+const passwordValidator = (element) => {
+    if (element.dataset.valEqualtoOther !== undefined) {
+        let password = document.getElementsByName(element.dataset.valEqualtoOther.replace('*', 'SignUpForm'))[0].value
+
+        if (element.value === password) {
+            formErrorHandler(element, true)
+        }
+        else {
+            formErrorHandler(element, false)
+        }
+    }
+    else {
+        const regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+        formErrorHandler(element, regEx.test(element.value))
+    }
+}
+
+const checkboxValidator = (element) => {
+    if (element.checked)
+        formErrorHandler(element, true)
+    else
+        formErrorHandler(element, false)
+}
+
+let forms = document.querySelectorAll('form')
+let inputs = forms[0].querySelectorAll('input')
+
+inputs.forEach(inputs => {
+    if (inputs.dataset.val === 'true') {
+        if (inputs.type === 'checkbox') {
+            inputs.addEventListener('change', (e) => {
+                checkboxValidator(e.target)
+            })
+        }
+        else {
+            inputs.addEventListener('keyup', (e) => {
+                switch (e.target.type) {
+                    case 'text':
+                        textValidator(e.target)
+                        break;
+
+                    case 'email':
+                        emailValidator(e.target)
+                        break;
+
+                    case 'password':
+                        passwordValidator(e.target)
+                        break;
+                }
+            })
+        }
+    }
+})
